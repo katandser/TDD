eeacf9cc519f83cbdcacf42f3b43ff01c435e873
vemce <katandser96@mail.ru>
2019-04-01 12:38:23 +0700

    succsesfully creating a type class "GeneratorPattern"

--- src/test/java/TestGeneratorPatterns.java
+++ src/test/java/TestGeneratorPatterns.java
@@ -3,6 +3,6 @@ import org.junit.Test;
 public class TestGeneratorPatterns {
     @Test
     public void initGenerator() {
-        GeneratorPatterns generatorPatterns;
+        GeneratorPatterns generatorPatterns = new GeneratorPatterns();
     }
 }
