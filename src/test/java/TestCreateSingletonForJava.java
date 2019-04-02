5b739c7d52776284d29a08ac13337c5008521102
vemce <katandser96@mail.ru>
2019-04-02 14:38:14 +0700

    attempt equals returing values from "createSingleton" with real example singleton

--- src/test/java/TestCreateSingletonForJava.java
+++ src/test/java/TestCreateSingletonForJava.java
@@ -8,5 +8,20 @@ public class TestCreateSingletonForJava {
         GeneratorPatternsJava generatorPatterns = new GeneratorPatternsJava();
         assertNotEquals(0, generatorPatterns.createSingleton().length());
     }
+    @Test
+    public void assertReturningValuesWithExample() {
+        String string = new String(" class Singleton {\n" +
+                "    private static Singleton instance;\n" +
+                "    private Singleton() {}\n" +
+                "    public static Singleton getInstance() {\n" +
+                "        if (instance == null) {\n" +
+                "            instance = new Singleton();\n" +
+                "        }\n" +
+                "        return instance;\n" +
+                "    }\n" +
+                "} ");
+        GeneratorPatternsJava generatorPatterns = new GeneratorPatternsJava();
+        assertEquals(string,generatorPatterns.createSingleton());
+    }
 
 }
