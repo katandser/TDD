159b89c7e02bbea3bd008cb4194e8f9ce4659c06
vemce <katandser96@mail.ru>
2019-04-02 14:24:24 +0700

    deleted test "createSingletonReturnsString" is break

--- src/test/java/TestCreateSingletonForJava.java
+++ src/test/java/TestCreateSingletonForJava.java
@@ -3,15 +3,10 @@ import static org.junit.Assert.*;
 
 public class TestCreateSingletonForJava {
     @Test
-    public void createSingletonReturnsString() {
-        String string = new String();
-        GeneratorPatternsJava generatorPatterns = new GeneratorPatternsJava();
-        assertEquals(string, generatorPatterns.createSingleton());
-    }
-    @Test
     public void createSingletonReturnsStringWithValue() {
         String string = new String();
         GeneratorPatternsJava generatorPatterns = new GeneratorPatternsJava();
         assertNotEquals(0, generatorPatterns.createSingleton().length());
     }
+
 }
