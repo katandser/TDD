b0936aa4aec3f7aceb5ff241b4f31aad8a7a5afc
vemce <katandser96@mail.ru>
2019-04-04 06:09:14 +0700

    attempt equals returing values from "createSingleton("name") with real examples with name "name"

--- src/test/java/TestCreateSingletonForJava.java
+++ src/test/java/TestCreateSingletonForJava.java
@@ -23,5 +23,20 @@ public class TestCreateSingletonForJava {
         GeneratorPatternsJava generatorPatterns = new GeneratorPatternsJava();
         assertEquals(string,generatorPatterns.createSingleton());
     }
+    @Test
+    public void assertReturningValuesWithExampleWhichTheHaveNameOf_name_() {
+        String string = new String(" class name {\n" +
+                "    private static Singleton instance;\n" +
+                "    private Singleton() {}\n" +
+                "    public static Singleton getInstance() {\n" +
+                "        if (instance == null) {\n" +
+                "            instance = new Singleton();\n" +
+                "        }\n" +
+                "        return instance;\n" +
+                "    }\n" +
+                "} ");
+        GeneratorPatternsJava generatorPatterns = new GeneratorPatternsJava();
+        assertEquals(string,generatorPatterns.createSingleton("name"));
+    }
 
 }
