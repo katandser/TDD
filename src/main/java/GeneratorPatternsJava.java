2d5bc6c818dc35ac7ffb201b551e11d94899f407
vemce <katandser96@mail.ru>
2019-04-04 06:34:23 +0700

    success attempt equals returing values from "createSingleton("Singleton") with real examples with name "Singleton"

--- src/main/java/GeneratorPatternsJava.java
+++ src/main/java/GeneratorPatternsJava.java
@@ -12,29 +12,15 @@ public class GeneratorPatternsJava extends GeneratorPatterns{
                 "} ";
     }
     public String createSingleton(String nameOfPattern) {
-        if (nameOfPattern.equals("name")) {
-            return  " class name {\n" +
-                    "    private static Singleton instance;\n" +
-                    "    private Singleton() {}\n" +
-                    "    public static Singleton getInstance() {\n" +
-                    "        if (instance == null) {\n" +
-                    "            instance = new Singleton();\n" +
-                    "        }\n" +
-                    "        return instance;\n" +
-                    "    }\n" +
-                    "} ";
-        }
-        else {
-            return  " class Server {\n" +
-                    "    private static Singleton instance;\n" +
-                    "    private Singleton() {}\n" +
-                    "    public static Singleton getInstance() {\n" +
-                    "        if (instance == null) {\n" +
-                    "            instance = new Singleton();\n" +
-                    "        }\n" +
-                    "        return instance;\n" +
-                    "    }\n" +
-                    "} ";
-        }
+        return  " class " + nameOfPattern + " {\n" +
+                "    private static Singleton instance;\n" +
+                "    private Singleton() {}\n" +
+                "    public static Singleton getInstance() {\n" +
+                "        if (instance == null) {\n" +
+                "            instance = new Singleton();\n" +
+                "        }\n" +
+                "        return instance;\n" +
+                "    }\n" +
+                "} ";
     }
 }
