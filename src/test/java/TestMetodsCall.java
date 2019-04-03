fc242b8f42e5c0864d17314785da4382e9d9e73d
vemce <katandser96@mail.ru>
2019-04-04 06:00:34 +0700

    attempt calling the metod "createSingleton("name")" from class GeneratorPatternsJava

--- src/test/java/TestMetodsCall.java
+++ src/test/java/TestMetodsCall.java
@@ -12,9 +12,14 @@ public class TestMetodsCall {
         generatorPatternsCPlusPlus.createSingleton();
     }
     @Test
-    public void callcallMetodCreateSingletonForUnknowGenerator() {
+    public void callMetodCreateSingletonForUnknowGenerator() {
         GeneratorPatterns[] arrayGeneratorPatterns = {new GeneratorPatternsCPlusPlus(), new GeneratorPatternsJava()};
         arrayGeneratorPatterns[0].createSingleton();
         arrayGeneratorPatterns[1].createSingleton();
+    }
+    @Test
+    public void callMetodCreateSingletonWithName_name_ForJava() {
+        GeneratorPatternsJava generatorPatternsJava = new GeneratorPatternsJava();
+        generatorPatternsJava.createSingleton("name");
     }
 }
