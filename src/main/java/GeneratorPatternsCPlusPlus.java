7a5b5c3ee3fd94ddcadac8027028c80dfddeb9d9
vemce <katandser96@mail.ru>
2019-04-04 06:44:59 +0700

    delete test createSingletonReturnsString()

--- src/main/java/GeneratorPatternsCPlusPlus.java
+++ src/main/java/GeneratorPatternsCPlusPlus.java
@@ -1,5 +1,20 @@
 public class GeneratorPatternsCPlusPlus extends GeneratorPatterns{
     public String createSingleton() {
-        return new String();
+        return "class Singleton\n" +
+                "{\n" +
+                "  private:\n" +
+                "    static Singleton * p_instance;\n" +
+                "    // Конструкторы и оператор присваивания недоступны клиентам\n" +
+                "    Singleton() {}\n" +
+                "    Singleton( const Singleton& );  \n" +
+                "    Singleton& operator=( Singleton& );\n" +
+                "  public:\n" +
+                "    static Singleton * getInstance() {\n" +
+                "        if(!p_instance)           \n" +
+                "            p_instance = new Singleton();\n" +
+                "        return p_instance;\n" +
+                "    }\n" +
+                "};\n" +
+                "Singleton* Singleton::p_instance = 0;";
     }
 }
