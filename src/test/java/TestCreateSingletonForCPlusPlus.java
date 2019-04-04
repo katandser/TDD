249ac8cd58b9ee59eb0feea5c7e8b1761757e5a3
vemce <katandser96@mail.ru>
2019-04-04 09:48:07 +0700

    failded test method createSingleton("Singleton") from class GeneratorPatternsCPlusPlus

--- src/test/java/TestCreateSingletonForCPlusPlus.java
+++ src/test/java/TestCreateSingletonForCPlusPlus.java
@@ -68,5 +68,28 @@ public class TestCreateSingletonForCPlusPlus {
         GeneratorPatternsCPlusPlus generatorPatterns = new GeneratorPatternsCPlusPlus();
         assertEquals(string,generatorPatterns.createSingleton("Human"));
     }
+    @Test
+    public void assertReturningValuesWithExampleWhichTheHaveNameOf_Singleton_() {
+        String string =
+                "class Singleton\n" +
+                        "{\n" +
+                        "  private:\n" +
+                        "    static Singleton * p_instance;\n" +
+                        "    // Конструкторы и оператор присваивания недоступны клиентам\n" +
+                        "    Singleton() {}\n" +
+                        "    Singleton( const Singleton& );  \n" +
+                        "    Singleton& operator=( Singleton& );\n" +
+                        "  public:\n" +
+                        "    static Singleton * getInstance() {\n" +
+                        "        if(!p_instance)           \n" +
+                        "            p_instance = new Singleton();\n" +
+                        "        return p_instance;\n" +
+                        "    }\n" +
+                        "};\n" +
+                        "Singleton* Singleton::p_instance = 0;";
+        GeneratorPatternsCPlusPlus generatorPatterns = new GeneratorPatternsCPlusPlus();
+        assertEquals(string,generatorPatterns.createSingleton("Singleton"));
+    }
+
 
 }
